4c8a789c77111db3c231d2c1ca43ff6c0a89d85e
Han <97804279+lh257796@users.noreply.github.com>
2025-02-26 22:43:01 +0000

    finishing touches on streamlit pages

--- Notebooks/pages/Bio.py
+++ Notebooks/pages/Bio.py
@@ -1,20 +1,15 @@
 import streamlit as st
 def main():
-    st.title("About me")
-    st.subheader("Academic Background")
-
-    st.write("""
-        Thank you for checking out my about me page!
-        """)
-    
+    st.title("About Me")
+    st.subheader("_Thank you for checking out my about me page!_")
     st.image('Notebooks/pages/selfie.jpg', width = 500)
 
     st.write("""
              
-        I'm currently a Master's student in Data Science at Eastern University. 
+        I'm currently a Masters student in Data Science at Eastern University. 
         I'm passionate about building machine learning models for healthcare 
         applications. One example is using computer vision--like this capstone, where I build medical imaging 
-        models to create predictions for different chest-related pathologies.
+        models to create predictions for different chest-related pathologies. :computer:
              
         I've always been interested in technology from a young age. As a middle schooler, I would procrastinate 
         studying by creating games and programs using BASIC on a TI-84 calculator, and sell popular games for money
@@ -22,17 +17,17 @@ def main():
         but still enjoyed my two courses in Python and web design. Most recently, I placed 3rd in a hackathon 
         creating a React Native application that utilizes NFC middleware to distribute meal-cards and enable
         restaurants/food pantries to efficiently provide meals to the needy and underserved populations. In my free time,
-        I enjoy volunteering with New York Cares to give back to my commmunity here in NYC! 
+        I enjoy volunteering with New York Cares to give back to my commmunity here in NYC!  :gift_heart:
              
-        I would love to work on creating machine learning models to improve our healthcare system. It's a dream of mine to 
+        I aspire to work on creating machine learning models that can improve our healthcare system. It's a dream of mine to 
         renovate American medicine in a way that provides medical accessibility (literacy and care access particularly) 
-        to every American, and eventually everyone in the world. I believe ML models are a means of providing this access, 
+        to every American, and eventually everyone in the world. I believe deep learning models are a means of providing this sort of healthcare access, 
         and I'm grateful for my time at Eastern for giving me the tools to learn how to apply such models for incredible usecases 
-        such as the one in my capstone project!
+        such as the one in my capstone project! :hospital: 
         
         - **Interests:** Machine Learning, Computer Vision, Healthcare Tech
         - **Hobbies:** Pickleball, Concerts, Fashion, Cooking, Reading
-        - **Unique Aspect:** I have previously started my own streetwear company and attended med school during the pandemic.
+        - **Unique Aspect:** I have previously started my own streetwear company :athletic_shoe: and attended medical school during the pandemic :male-doctor:.
              
         Thank you for reading and checking out my website and biography. If you're interested in learning more about me, check out 
         the sidebar to keep browsing!
--- Notebooks/pages/Other Projects.py
+++ Notebooks/pages/Other Projects.py
@@ -5,12 +5,14 @@ def main():
     st.write("Most of my other completed projects can be found at my [personal website](https://hanlu.dev).")
     st.write("Examples of things I've worked on:")
     st.markdown("""
-         - IoTs (Smart Mirror) 
+         - IoTs (Smart Mirror with Raspberry Pi  (_picture below_!)) 
          - React Native application incorporating NFT middleware for a Hackathon placing 3rd
-         - Platformer game designed in Unreal Engine 5
+         - 3D Platformer game designed in Unreal Engine 5
+         - Reservation system built in Python + Django for music venues
+         - Dog walking website built in React (PERN stack) with functioning database
     """)
 
-    st.write("For other work in progress projects or to see some of my repos in more depth, check out my [Github](https://github.com/lh25779)!")
-
+    st.write("For other work in progress projects or to see some of my repos in more depth, including the one for this project, check out my [Github](https://github.com/lh25779)!")
+    st.image("Notebooks/pages/mirror.jpg")
 if __name__ == "__main__":
     main()
